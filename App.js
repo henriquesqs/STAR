5185f21204e792a3e0f429e498f7d7f172083d1e
henriquesqs <henriquesqs@hotmail.com>
2020-11-10 18:29:51 -0300

    Added option headerShown false to HomeScreen

--- App.js
+++ App.js
@@ -11,67 +11,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 
 const App = () => {
+
   const Stack = createStackNavigator();
+
   return (
     <NavigationContainer>
-
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-          name="Home"
-          component={Homescreen}
-        />
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Homescreen} />
         <Stack.Screen name="About" component={Aboutscreen} />
       </Stack.Navigator>
-      
     </NavigationContainer>
   );
 };
 
 export default App;
-
-// In App.js in a new project----------------------------------
-
-/* import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App; */
