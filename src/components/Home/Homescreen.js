cd505a2554f20cef1a900e6d5764272c03651682
henriquesqs <henriquesqs@hotmail.com>
2020-11-10 17:27:10 -0300

    Added new icons to homeScreen

--- src/components/Home/Homescreen.js
+++ src/components/Home/Homescreen.js
@@ -1,37 +1,37 @@
 import React from 'react';
-import { View,Text , TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import Button from "../Button/Button.js";
 
 import styles from "./styles";
 
-const Homescreen = ( {navigation} ) =>{
+const Homescreen = ({ navigation }) => {
   return (
     <View style={styles.containerPrincipal}>
       <View style={styles.containerContent}>
         <View style={styles.textContent}>
-          <Text style={{fontSize: 25}}>IOT PROJECT</Text>
+          <Text style={{ fontSize: 25 }}>IOT PROJECT</Text>
         </View>
 
         <View style={styles.imagesIcons}>
           <Image
-            style={{width:50,height:50}}
-            source={require("../../../assets/icons/png/001-tablet.png")}/>
+            style={{ width: 50, height: 50 }}
+            source={require("../../../assets/icons/security.png")} />
           <Image
-            style={{width:50,height:50}}
-            source={require("../../../assets/icons/png/006-iphone-1.png")}/>
+            style={{ width: 50, height: 50 }}
+            source={require("../../../assets/icons/phone.png")} />
           <Image
-            style={{width:50,height:50}}
-            source={require("../../../assets/icons/png/025-pc-screen.png")}/>
+            style={{ width: 50, height: 50 }}
+            source={require("../../../assets/icons/computer.png")} />
           <Image
-            style={{width:50,height:50}}
-            source={require("../../../assets/icons/png/casa.png")}/>
+            style={{ width: 50, height: 50 }}
+            source={require("../../../assets/icons/home.png")} />
         </View>
-        
+
       </View>
-      
+
       <View style={styles.containerButtons}>
-        <Button title="CONNECT"/>
-        <Button title="SOBRE" onPress={() => navigation.navigate('About')}/>
+        <Button title="CONNECT" />
+        <Button title="SOBRE" onPress={() => navigation.navigate('About')} />
       </View>
     </View>
   );
