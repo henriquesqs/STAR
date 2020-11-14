55982e0fb88ea2f19c84f1d1efd2a0b52b9c20b5
Gustavo <gumastro@usp.br>
2020-11-13 23:15:14 -0300

    adds Criadores button on About section

--- src/components/AboutTeam/AboutTeam.js
+++ src/components/AboutTeam/AboutTeam.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Text, View, Image, ViewPagerAndroidBase } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, Image, ViewPagerAndroidBase } from 'react-native';
 import BackArrow from "../../../assets/icons/back.png"
+import People from "../../../assets/icons/people.png";
 import {
   useFonts,
   Roboto_100Thin,
@@ -21,14 +22,72 @@ import styles from "./styles.js";
 import Navbar from '../Navbar/Navbar';
 
 const AboutTeam = (props) => {
+
+  const [isAuthorsFocused, setisAuthorsFocused] = useState(false);
+
+  function focusAuthors() {
+
+    setisAuthorsFocused(!isAuthorsFocused);
+
+    if (!isAuthorsFocused) {
+      // setFirstFlex(0.75);
+      //  setSecondFlex(0);
+      // setSecondDisplay('none');
+    }
+    else {
+      // styles.authors.flex = 0.15,
+      // setFirstFlex(0.15);
+      // setSecondFlex(0.15);
+      // setSecondDisplay('flex');
+    }
+  }
+
   return (
     <>
-      <Navbar title="About us" description="Informations about this app" />
+      <Navbar
+        title="About us"
+        description="Informations about this app"
+      />
       <View style={styles.containerContent}>
-        <Text>about?</Text>
+        <TouchableOpacity style={isAuthorsFocused? styles.authorsTitleFocused : styles.authors} onPress={focusAuthors}>
+          <Image source={require('../../../assets/icons/people.png')}
+            style={{
+              width: 50,
+              height: 50,
+              marginTop: 0,
+              marginLeft: 35,
+              marginRight: 20,
+              left: 0,
+              top: 0,
+              tintColor: 'white',
+              resizeMode: 'contain',
+            }}
+          />
+          <Text style={styles.authorsButton }>CRIADORES</Text>
+
+        </TouchableOpacity>
+        {isAuthorsFocused && (
+            <View style={styles.authorsFocused}>
+              <Text style={styles.authorsInfoTitleMain}>BACKEND</Text>
+              <Text style={styles.authorsInfoName}>PESSOA 1</Text>
+              <Text style={styles.authorsInfoUsp}>NUSP 00000000</Text>
+              <Text style={styles.authorsInfoName}>PESSOA 2</Text>
+              <Text style={styles.authorsInfoUsp}>NUSP 00000000</Text>
+              <Text style={styles.authorsInfoName}>PESSOA 3</Text>
+              <Text style={styles.authorsInfoUsp}>NUSP 00000000</Text>
+              <Text style={styles.authorsInfoTitle}>FRONTEND</Text>
+              <Text style={styles.authorsInfoName}>PESSOA 1</Text>
+              <Text style={styles.authorsInfoUsp}>NUSP 00000000</Text>
+              <Text style={styles.authorsInfoName}>PESSOA 2</Text>
+              <Text style={styles.authorsInfoUsp}>NUSP 00000000</Text>
+              <Text style={styles.authorsInfoName}>PESSOA 3</Text>
+              <Text style={styles.authorsInfoUsp}>NUSP 00000000</Text>
+            </View>
+            
+          )}
       </View>
     </>
   );
-}
+};
 
 export default AboutTeam;
