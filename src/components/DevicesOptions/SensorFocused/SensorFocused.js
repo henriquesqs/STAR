ec65ffcc1c3f8469e75fdc5a042b043000b5cc95
henriquesqs <henriquesqs@hotmail.com>
2020-12-03 20:24:16 -0300

    Finished Sensor focused card

--- src/components/DevicesOptions/SensorFocused/SensorFocused.js
+++ src/components/DevicesOptions/SensorFocused/SensorFocused.js
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
 import { Text, View, Image } from "react-native";
-import PowerIcon from "../../../../assets/icons/power.png"
-import LowTempIcon from "../../../../assets/icons/low-temp.png";
-import HighTempIcon from "../../../../assets/icons/high-temp.png";
-import TempNowIcon from "../../../../assets/icons/temp-now.png";
-import NoPeopleIcon from "../../../../assets/icons/no-people.png";
-import ClockIcon from "../../../../assets/icons/clock.png";
 import NewButton from "../../Button/NewButton.js";
-import CircularButton from "../../CircularButton/CircularButton.js";
+import TempSensorIcon from "../../../../assets/icons/TempSensor.png";
+import LightSensorIcon from "../../../../assets/icons/LightSensor.png";
+import MotionSensorIcon from "../../../../assets/icons/MotionSensor.png";
 
 import styles from "./styles.js";
 
@@ -23,33 +19,22 @@ const SensorFocused = (props) => {
         <View style={styles.firstdescription}>
           <View style={styles.imageBlock}>
             <Image
-              source={PowerIcon}
+              source={TempSensorIcon}
               width={0}
               height={0}
               style={{
                 resizeMode: "contain",
                 width: 50,
                 height: 45,
+                tintColor: "white"
               }}
             />
           </View>
 
           <View style={{ flexDirection: "column", flex: 0.65 }}>
-            <Text style={styles.state}>STATE:</Text>
+            <Text style={styles.state}>TEMPERATURE:</Text>
             <Text style={styles.stateMode}>teste</Text>
           </View>
-        </View>
-        <View style={styles.boxbuttons}>
-          <NewButton
-            width={100}
-            height={50}
-            bgColor={"#454ADE"}
-            bRadius={10}
-            title={"teste"}
-            txtColor="white"
-            txtSize={13}
-            marginTop={"16%"}
-          ></NewButton>
         </View>
       </View>
 
@@ -57,44 +42,20 @@ const SensorFocused = (props) => {
         <View style={styles.firstdescription}>
           <View style={styles.imageBlock}>
             <Image
-              source={LowTempIcon}
+              source={LightSensorIcon}
               width={0}
               height={0}
               style={{
                 resizeMode: "contain",
                 width: 50,
                 height: 45,
-                // tintColor: iconsColor,
+                tintColor: "white",
               }}
             />
           </View>
           <View style={{ flexDirection: "column", flex: 0.65 }}>
-            <Text style={styles.state}>MIN TEMP.:</Text>
+            <Text style={styles.state}>LUMINOSITY:</Text>
             <Text style={styles.stateMode}>teste</Text>
-          </View>
-        </View>
-        <View style={styles.boxbuttons}>
-          <View style={styles.doublebutton}>
-            <CircularButton
-              width={45}
-              height={45}
-              bgColor={"#454ADE"}
-              bRadius={100}
-              title="+"
-              txtColor="white"
-              txtSize={30}
-              marginTop={"5%"}
-            />
-            <CircularButton
-              width={45}
-              height={45}
-              bgColor={"#454ADE"}
-              bRadius={100}
-              title="-"
-              txtColor="white"
-              txtSize={30}
-              marginTop={"0%"}
-            />
           </View>
         </View>
       </View>
@@ -103,169 +64,37 @@ const SensorFocused = (props) => {
         <View style={styles.firstdescription}>
           <View style={styles.imageBlock}>
             <Image
-              source={HighTempIcon}
+              source={MotionSensorIcon}
               width={0}
               height={0}
               style={{
                 resizeMode: "contain",
                 width: 50,
                 height: 45,
+                tintColor: "white",
               }}
             />
           </View>
           <View style={{ flexDirection: "column", flex: 0.65 }}>
-            <Text style={styles.state}>MAX TEMP.:</Text>
+            <Text style={styles.state}>MOTION:</Text>
             <Text style={styles.stateMode}>teste</Text>
-          </View>
-        </View>
-        <View style={styles.boxbuttons}>
-          <View style={styles.doublebutton}>
-            <CircularButton
-              width={45}
-              height={45}
-              bgColor={"#454ADE"}
-              bRadius={100}
-              title="+"
-              txtColor="white"
-              txtSize={30}
-              marginTop={"5%"}
-            />
-            <CircularButton
-              width={45}
-              height={45}
-              bgColor={"#454ADE"}
-              bRadius={100}
-              title="-"
-              txtColor="white"
-              txtSize={30}
-              marginTop={"0%"}
-            />
           </View>
         </View>
       </View>
 
       <View style={styles.cardFourthIcon}>
-        <View style={styles.firstdescription}>
-          <View style={styles.imageBlock}>
-            <Image
-              source={TempNowIcon}
-              width={0}
-              height={0}
-              style={{
-                resizeMode: "contain",
-                width: 50,
-                height: 45,
-              }}
-            />
-          </View>
-          <View style={{ flexDirection: "column", flex: 0.65 }}>
-            <Text style={styles.state}>TEMP. NOW:</Text>
-            <Text style={styles.stateMode}>teste</Text>
-          </View>
-        </View>
-        <View style={styles.boxbuttons}>
-          <View style={styles.doublebutton}>
-            <CircularButton
-              width={45}
-              height={45}
-              bgColor={"#454ADE"}
-              bRadius={100}
-              title="+"
-              txtColor="white"
-              txtSize={30}
-              marginTop={"5%"}
-            />
-            <CircularButton
-              width={45}
-              height={45}
-              bgColor={"#454ADE"}
-              bRadius={100}
-              title="-"
-              txtColor="white"
-              txtSize={30}
-              marginTop={"0%"}
-            />
-          </View>
-        </View>
+        <NewButton
+          width={100}
+          height={50}
+          bgColor={"#454ADE"}
+          bRadius={10}
+          title={"ATUALIZAR"}
+          txtColor="white"
+          txtSize={13}
+          marginTop={"16%"}
+        ></NewButton>
       </View>
 
-      <View style={styles.cardFifthIcon}>
-        <View style={styles.firstdescription}>
-          <View style={styles.imageBlock}>
-            <Image
-              source={NoPeopleIcon}
-              width={0}
-              height={0}
-              style={{
-                resizeMode: "contain",
-                width: 50,
-                height: 45,
-              }}
-            />
-          </View>
-          <View style={{ flexDirection: "column", flex: 0.65 }}>
-            <Text style={styles.state}>W/ NOBODY:</Text>
-            <Text style={styles.stateMode}>teste</Text>
-          </View>
-        </View>
-        <View style={styles.boxbuttons}>
-          <NewButton
-            width={100}
-            height={50}
-            bgColor={"#454ADE"}
-            bRadius={10}
-            title={"teste"}
-            txtColor="white"
-            txtSize={13}
-            marginTop={"16%"}
-          ></NewButton>
-        </View>
-      </View>
-
-      <View style={styles.cardSixthIcon}>
-        <View style={styles.firstdescription}>
-          <View style={styles.imageBlock}>
-            <Image
-              source={ClockIcon}
-              width={0}
-              height={0}
-              style={{
-                resizeMode: "contain",
-                width: 50,
-                height: 45,
-              }}
-            />
-          </View>
-          <View style={{ flexDirection: "column", flex: 0.65 }}>
-            <Text style={styles.state}>WAIT TIME:</Text>
-            <Text style={styles.stateMode}>teste</Text>
-          </View>
-        </View>
-        <View style={styles.boxbuttons}>
-          <View style={styles.doublebutton}>
-            <CircularButton
-              width={45}
-              height={45}
-              bgColor={"#454ADE"}
-              bRadius={100}
-              title="+"
-              txtColor="white"
-              txtSize={30}
-              marginTop={"5%"}
-            />
-            <CircularButton
-              width={45}
-              height={45}
-              bgColor={"#454ADE"}
-              bRadius={100}
-              title="-"
-              txtColor="white"
-              txtSize={30}
-              marginTop={"0%"}
-            />
-          </View>
-        </View>
-      </View>
     </View>
   );
 };
